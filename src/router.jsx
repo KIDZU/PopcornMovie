8983bd343f557f4453fa2adcc585ba08db7dbd32
ALEX.V <trofim.ivanchenko@nordclan.com>
2025-04-01 01:53:16 +0300

    add deploy

--- src/router.jsx
+++ src/router.jsx
@@ -1,17 +1,23 @@
-import { BrowserRouter, Route, Routes, useParams } from 'react-router-dom'
+import {
+  BrowserRouter,
+  HashRouter,
+  Route,
+  Routes,
+  useParams,
+} from 'react-router-dom'
 import { Layout } from './Components/Layout/Layout'
 import { Main } from './Pages/Main/Main'
 import { Movie } from './Pages/Movie/Movie'
 
 export const Router = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route exact path="/:name?" element={<Main />} />
           <Route path="/movie/:id" element={<Movie />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
