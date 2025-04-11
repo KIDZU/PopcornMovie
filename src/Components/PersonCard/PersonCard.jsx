39c2f35b964f497644b294b1c0e0e834e15a4eeb
ALEX.V <trofim.ivanchenko@nordclan.com>
2025-04-11 20:06:50 +0300

    Fix person card

--- src/Components/PersonCard/PersonCard.jsx
+++ src/Components/PersonCard/PersonCard.jsx
@@ -1,11 +1,12 @@
 import { NavLink } from 'react-router-dom'
 import style from './PersonCard.module.css'
+import { Image } from 'antd'
 export function PersonCard({ personData }) {
   const { enName, name, photo, profession, enProfession, id } = personData
 
   return (
     <NavLink className={style.blockPerson} to={`/personInfo/${id}`}>
-      <img src={photo} alt="photoPerson" />
+      <Image src={photo} alt="person photo" height={'100%'} width={100} />
       <div>
         <p className={style.personName}>{name ? name : enName}</p>
         <p>{profession ? profession : enProfession}</p>
