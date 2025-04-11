e14087c649337cd27392a574d6b7db9b1617d3a6
ALEX.V <trofim.ivanchenko@nordclan.com>
2025-04-11 19:51:31 +0300

    Movie page update

--- src/Components/PersonCard/PersonCard.jsx
+++ src/Components/PersonCard/PersonCard.jsx
@@ -1,25 +1,15 @@
-
-import { NavLink } from "react-router-dom"
-import style from "./PersonCard.module.css"
-export function PersonCard ({personData}) {
-  const {
-    enName,
-    name,
-    photo,
-    profession,
-    enProfession,
-    id
-  } = personData
+import { NavLink } from 'react-router-dom'
+import style from './PersonCard.module.css'
+export function PersonCard({ personData }) {
+  const { enName, name, photo, profession, enProfession, id } = personData
 
   return (
-    <NavLink to={`/personInfo/${id}`}>
-    <div className={style.blockPerson}>
-    <img src={photo} alt="photoPerson" />
-    <div>
-   <p className={style.personName}>{name ? name : enName}</p>
-   <p>{profession ? profession : enProfession}</p>
-   </div>
-   </div>
-</NavLink>
+    <NavLink className={style.blockPerson} to={`/personInfo/${id}`}>
+      <img src={photo} alt="photoPerson" />
+      <div>
+        <p className={style.personName}>{name ? name : enName}</p>
+        <p>{profession ? profession : enProfession}</p>
+      </div>
+    </NavLink>
   )
 }
--- src/Pages/Movie/Movie.jsx
+++ src/Pages/Movie/Movie.jsx
@@ -6,25 +6,21 @@ import { Image } from 'antd'
 import { getMovieData } from '../../api/getMovieData.js'
 import { PersonCard } from '../../Components/PersonCard/PersonCard.jsx'
 
-
-
 const initState = {
   name: '',
   poster: { url: '' },
   rating: { imdb: 0 },
   id: 0,
   description: '',
-  persons:[]
+  persons: [],
 }
 
 export const Movie = () => {
   const [movieData, setMovieData] = useState(initState)
   const { id } = useParams()
 
-
-
   useEffect(() => {
-    getMovieData(id).then((data)=>{
+    getMovieData(id).then((data) => {
       setMovieData(data)
     })
   }, [])
@@ -34,38 +30,37 @@ export const Movie = () => {
     rating: { imdb },
     poster: { url },
     description,
-    persons
+    persons,
   } = movieData
 
   const personsByProf = {}
-persons.forEach((person)=>{
-  const key = person.enProfession
-if (personsByProf[key]){
-  personsByProf[key].push(person)
-}
-else{
-  personsByProf[key] = [person]
-}
-})
-
-function getRenderPersonsByProf(){
-  return Object.keys(personsByProf).map((key)=>{
-    const {profession, enProfession} = personsByProf[key][0]
-    const title = profession ?? enProfession
-    return (
-      <div>
-        <h4>
-          {title}
-        </h4>
-        <div>
-          {personsByProf[key].map((personData)=>( 
-          <PersonCard personData={personData} key={personData.id} />
-  ))}
-        </div>
-      </div>
-    )
+  persons.forEach((person) => {
+    const key = person.enProfession
+    if (personsByProf[key]) {
+      personsByProf[key].push(person)
+    } else {
+      personsByProf[key] = [person]
+    }
   })
-}
+
+  function getRenderPersonsByProf() {
+    return Object.keys(personsByProf)
+      .sort((a, b) => (a > b ? 1 : -1))
+      .map((key) => {
+        const { profession, enProfession } = personsByProf[key][0]
+        const title = profession ?? enProfession
+        return (
+          <div>
+            <h4>{title}</h4>
+            <div className={style.personCardList}>
+              {personsByProf[key].map((personData) => (
+                <PersonCard personData={personData} key={personData.id} />
+              ))}
+            </div>
+          </div>
+        )
+      })
+  }
 
   return (
     <Container>
@@ -79,19 +74,18 @@ function getRenderPersonsByProf(){
           />
         </div>
         <div className={style.movieBlockText}>
-
           <p className={style.movieTitle}>{name}</p>
-        <div className={style.tableInfoMovie}>
-              <p>Рейтинг: {imdb ? imdb : "недостаточно оценок"}</p>
-        </div>
+          <div className={style.tableInfoMovie}>
+            <p>Рейтинг: {imdb ? imdb : 'недостаточно оценок'}</p>
+          </div>
           <p>{description}</p>
         </div>
       </div>
-      
+
       <div className={style.personsListContainer}>
-      <h3>Актёрский состав</h3>
-     <div className={style.personsList}>{getRenderPersonsByProf()}</div>
-     </div>
+        <h3>В создании фильма принимали участие:</h3>
+        <div className={style.personsList}>{getRenderPersonsByProf()}</div>
+      </div>
     </Container>
   )
 }
